5dd86d8b358a6a570a678a678eb354f293475024
p2783882 <p2783882@GWHXX681WIN008W>
2025-05-02 13:09:54 +0100

    Added testing for orders class

---
+++ Testing2/tstOrders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create instance of class
+            clsOrders AnOrder = new clsOrders();
+            //does it exist
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}

d2c05d8ecb64aeef02a28f51fc4136dc2b8b3653
LexCodes <aleksejevasandra@gmail.com>
2025-05-03 10:52:49 +0100

    Added testing for address class

---
+++ Testing3/tstProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstProduct
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance
+            clsProduct Anroduct = new clsProduct();
+
+            //test that it exists
+            Assert.IsNotNull(Anroduct);
+
+        }
+    }
+}

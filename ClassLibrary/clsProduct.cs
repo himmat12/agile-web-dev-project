22295c859204b0d4070b4eb830e2950545801053
P2763935 <P2763935@GWHXX682WIN013W>
2025-05-08 12:44:58 +0100

    updated table, added datetime. therefore, had to update the tests

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -2,5 +2,12 @@
 {
     public class clsProduct
     {
+        public bool InStock { get; set; }
+        public string releasedDate { get; set; }
+        public int ProductID { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
     }
 }

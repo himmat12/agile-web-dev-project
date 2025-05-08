73d5523ffdab7a39714d74f9f2d264b1cecc57dd
Maya2762008 <P2762008@my365.dmu.ac.uk>
2025-05-08 11:46:07 +0100

    I added testing

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public int staffId;
+        public int StaffId { get; set; }
+        public bool Active { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string ContactNumber { get; set; }
+
+    }
+}

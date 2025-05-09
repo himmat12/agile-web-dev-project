b258b36113d9862c5bf8f907a441aa19ed15fae8
Himmat <himmatrai20@gmail.com>
2025-05-09 13:37:02 +0100

    catchup with week 4 prac. 7 - all test cases for my table added

--- Testing1/tstSupplier.cs
+++ Testing1/tstSupplier.cs
@@ -13,5 +13,65 @@ namespace Testing1
             clsSupplier Supplier = new clsSupplier();
             Assert.IsNotNull(Supplier);
         }
+
+        [TestMethod]
+        public void ActivePropertyOk() {
+            clsSupplier Supplier = new clsSupplier();   
+            Boolean TestData = true;
+            Supplier.Active = TestData;
+
+            Assert.AreEqual(Supplier.Active,TestData);
+        }
+
+        [TestMethod]
+        public void NamePropertyOk()
+        {
+            clsSupplier Supplier = new clsSupplier();
+            String TestData = "Bob";
+            Supplier.Name = TestData;
+
+            Assert.AreEqual(Supplier.Name, TestData);
+        }
+
+        [TestMethod]
+        public void EmailPropertyOk()
+        {
+            clsSupplier Supplier = new clsSupplier();
+            String TestData = "bob2025@gmail.com";
+            Supplier.Email = TestData;
+
+            Assert.AreEqual(Supplier.Email, TestData);
+        }
+
+        [TestMethod]
+        public void PhonePropertyOk()
+        {
+            clsSupplier Supplier = new clsSupplier();
+            String TestData = "123456789";
+            Supplier.Phone = TestData;
+
+            Assert.AreEqual(Supplier.Phone, TestData);
+        }
+
+        [TestMethod]
+        public void AddressPropertyOk()
+        {
+            clsSupplier Supplier = new clsSupplier();
+            String TestData = "Gateway House, LE19BH";
+            Supplier.Address = TestData;
+
+            Assert.AreEqual(Supplier.Address, TestData);
+        }
+
+
+        [TestMethod]
+        public void DatePropertyOk()
+        {
+            clsSupplier Supplier = new clsSupplier();
+            DateTime TestData = DateTime.Now.Date;
+            Supplier.DateAdded = TestData;
+
+            Assert.AreEqual(Supplier.DateAdded, TestData);
+        }
     }
 }

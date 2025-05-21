04f3fd2f903f875c45ed590d8b0f89287e58944b
Himmat <himmatrai20@gmail.com>
2025-05-21 23:59:58 +0100

    lab 14a completed - delete test method added, and delete feature implemented in presentation and class supplier

--- AdminSystem/SupplierConfirmDelete.aspx.cs
+++ AdminSystem/SupplierConfirmDelete.aspx.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    Int32 SupplierId;
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        SupplierId = Convert.ToInt32(Session["SupplierId"]);
+    }
+
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        clsSupplierCollection SupplierList = new clsSupplierCollection();
+
+        SupplierList.ThisSupplier.Find(SupplierId);
+
+        SupplierList.Delete(SupplierId);
+
+        Response.Redirect("SupplierList.aspx");
+    }
+
+    protected void btnNo_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("SupplierList.aspx");
 
     }
 }
